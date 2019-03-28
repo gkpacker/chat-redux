e0257c9c26f79d919c599bb41103e22cf77fc69d
Gabriel Packer <ggkpacker@gmail.com>
2019-03-28 01:34:16 -0300

    Add create message action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 export const FECTH_MESSAGES = 'FECTH_MESSAGES';
+export const CREATE_MESSAGE = 'CREATE_MESSAGE';
 
 export function fetchMessages(channel) {
   const promise = fetch(`https://wagon-chat.herokuapp.com/${channel}/messages`)
@@ -6,6 +7,19 @@ export function fetchMessages(channel) {
 
   return {
     type: FECTH_MESSAGES,
+    payload: promise
+  };
+}
+
+export function createMessage(channel, author, content) {
+  const body = JSON.stringify({ author, content })
+  const promise = fetch(
+    `https://wagon-chat.herokuapp.com/${channel}/messages`,
+    { method: 'POST', body }
+  ).then(response => response.json());
+
+  return {
+    type: CREATE_MESSAGE,
     payload: promise
   };
 }
