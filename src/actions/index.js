1a193f7d7d371d17403473ff737fa1d576eedaff
Gabriel Packer <ggkpacker@gmail.com>
2019-03-28 01:34:02 -0300

    Add fetch messages action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,11 @@
-// TODO: add and export your own actions
+export const FECTH_MESSAGES = 'FECTH_MESSAGES';
+
+export function fetchMessages(channel) {
+  const promise = fetch(`https://wagon-chat.herokuapp.com/${channel}/messages`)
+    .then(response => response.json());
+
+  return {
+    type: FECTH_MESSAGES,
+    payload: promise
+  };
+}
