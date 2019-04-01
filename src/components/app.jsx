0e81c076f92b32656f2747b6df79e7b3ea329bcf
Gabriel Packer <ggkpacker@gmail.com>
2019-04-01 18:13:43 -0300

    Add logo & main components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import MessageList from '../containers/message_list';
+import ChannelList from '../containers/channel_list';
 
 const App = () => {
   return (
-    <div className="app">
-      <p>React + Redux starter</p>
+    <div className="app messaging-wrapper">
+      <div className="logo-container">
+        <img src="../../assets/images/lewagon_logo.png" alt="" className='messaging-logo'/>
+      </div>
+      <ChannelList />
+      <MessageList />
     </div>
   );
 };
