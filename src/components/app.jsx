83257471c4dd9a1528c83e5ef385d1696e8caeba
Gabriel Packer <ggkpacker@gmail.com>
2019-04-04 11:10:12 -0300

    Change lewagon logo

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="app messaging-wrapper">
       <div className="logo-container">
-        <img src="assets/images/lewagon_logo.png" alt="" className="messaging-logo" />
+        <img src="assets/images/lewagon_logo.svg" alt="logo" />
       </div>
       <ChannelList />
       <MessageList />
